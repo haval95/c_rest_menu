37cd8a74c588d7809a22279120806ed1f4071dc7
haval95 <havalrezan22@gmail.com>
2023-12-28 16:57:30 +0100

    single and multiple views applied

--- c_rest_menu/restaurant/views.py
+++ c_rest_menu/restaurant/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import generics, permissions, status
 from .models import Booking, Menu
 from .serializers import booking_serializer, menu_serializer
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
@@ -12,6 +13,16 @@ class booking_view(generics.ListCreateAPIView):
     serializer_class = booking_serializer
 
 
+class single_booking_item(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = booking_serializer
+
+
 class menu_view(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menu_serializer
+
+
+class single_menu_item(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = menu_serializer
